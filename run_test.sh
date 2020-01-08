7cd7a30b959fa12b1154b22034c759a53f0481a7
NajlaBH-QLdjango <bhnajla_bi@outlook.fr>
2020-01-08 18:05:14 +0000

    :wrench: Add chmod to run_test.

--- run_test.sh
+++ run_test.sh
@@ -5,28 +5,25 @@ err=0
 trap 'err=1' ERR
 
 # Clean
-if psql -lqt | cut -d \| -f 1 | grep -qw best_wishes ; then
+if psql -lqt | cut -d \| -f 1 | grep -qw bestwishesdb ; then
     read -p "Database 'best_wishes' required for running the tests already exist. Do you want to delete it (y)?" yn
     if echo "$yn" | grep -iq "^n" ;then
         exit
     else
-        dropdb best_wishes
+        dropdb bestwishesdb
     fi
 fi
 
-rm -rf gqt-dg-tmp/;
+rm -rf ./gql-dg-tmp;
 
-# Generate new code, (it also creates db, migrate and install dependencies)
-yes 'y' | cookiecutter . --no-input
+
 
 # Run the tests present inside generate project
-cd gqt-dg-tmp;
-npm run build
+cd ./gql-dg-tmp;
+python3 -m venv venv
 source venv/bin/activate
-ansible-playbook -i provisioner/hosts provisioner/site.yml --syntax-check
-fab test:"--cov"
 
 # Cleanup
-test ! $CI && dropdb best_wishes
+test ! $CI && dropdb bestwishesdb
 
 test $err = 0 # Return non-zero if any command failed
