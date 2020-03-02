82079bd3f22b0a8c12a5bd3b0f842303b0964f81
NajlaBH-QLdjango <bhnajla_bi@outlook.fr>
2020-03-02 13:15:06 +0000

    :ambulance: [fix-vulnerability] django 2.2.9 to 2.2.10 travis2

--- run_test.sh
+++ run_test.sh
@@ -1,41 +1,21 @@
 #!/bin/bash
-
+  
 # Setup for returning a non-zero exit code if any of the command fails.
 err=0
 trap 'err=1' ERR
 
-# Clean
-if psql -lqt | cut -d \| -f 1 | grep -qw circle_test ; then
-    read -p "Database 'circle_test' required for running the tests already exist. Do you want to delete it (y)?" yn
-    if echo "$yn" | grep -iq "^n" ;then
-        exit
-    else
-        dropdb circle_test
-    fi
-fi
 
-rm -rf ./gql-dg-tmp;
-
-#Create db demo-credits for travis
-psql -c "CREATE DATABASE circle_test;"
-psql -c "CREATE USER root WITH ENCRYPTED PASSWORD '';"
-psql -c "ALTER ROLE root SET client_encoding TO 'utf8';"
-psql -c "ALTER ROLE root SET default_transaction_isolation TO 'read committed';"
-psql -c "ALTER ROLE root SET timezone TO 'UTC';"
-psql -c "GRANT ALL PRIVILEGES ON DATABASE circle_test TO root;"
-#CircleCi user grant
-#psql -c "CREATE USER circleci WITH ENCRYPTED PASSWORD 'circleci';"
-#psql -c "ALTER ROLE circleci SET client_encoding TO 'utf8';"
-#psql -c "ALTER ROLE circleci SET default_transaction_isolation TO 'read committed';"
-#psql -c "ALTER ROLE circleci SET timezone TO 'UTC';"
-#psql -c "GRANT ALL PRIVILEGES ON DATABASE circle_test TO circleci;"
-
-# Run the tests present inside generate project
-python3 -m venv venv
-source venv/bin/activate
+#Run the tests present inside generate project
+python3 -m venv env
+source env/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py migrate
+#python manage.py migrate
+
+
+#Run tests
 python manage.py test
+
 
 # Cleanup
 test ! $CI && dropdb circle_test
